f00d161e6ae46687adbb96d74353e07512c874cf
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-09-22 10:39:35 +0300

    rectificiation: model fillable ecole

--- app/Models/Ecole.php
+++ app/Models/Ecole.php
@@ -13,4 +13,28 @@ class Ecole extends Model
     use HasFactory;
 
     protected $dates = ['deleted_at'];
+
+    protected $table = 'ecoles';
+
+    /*
+    * fillable
+    */
+    protected $fillable = [
+        'ecole_name',
+        'ecole_sigle',
+        'cle_unique',
+        'ecole_type',
+        'ecole_statut',
+        'ecole_logo',
+        'ecole_phone_fixe',
+        'ecole_phone_mobile',
+        'ecole_email',
+        'ecole_email_alternative',
+        'province_id',
+        'region_id',
+        'district_id',
+        'commune_id',
+        'ecole_adresse',
+        'user_id'
+    ];
 }
